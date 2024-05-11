a44317cd574d9507f58f39c669124f6978122c4f
andrzej-herman <andrzejek.herman@gmail.com>
2024-05-11 17:33:19 +0200

    Finish all

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
+// test
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
